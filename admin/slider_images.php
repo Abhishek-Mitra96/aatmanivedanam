7475a0890348605ae84c78e3782f943f98aedc5b
Abhishek Mitra <abhoffice99@gmail.com>
2017-06-09 18:13:53 +0530

    searching api

---
+++ admin/slider_images.php
@@ -0,0 +1,229 @@
+<?php 
+
+require_once ('theme/header_1.php'); 
+require_once ('theme/header_2.php'); 
+require_once ('theme/sidebar.php');
+?>
+</section>
+
+<div class="content-wrapper">
+      
+  <section class="content-header">
+    <h1> Slider Images</h1>
+    <!--<a href="image_add.php" class="btn btn-info pull-right" role="button">Add Images</a> -->
+  </section>
+      
+  <section class="content">
+    <div class="box box-primary" id="box-widget">
+      <div class="box-header">
+        <div class="row">
+          <!-- <div class="col-xs-12 col-sm-5">
+           <input class="Filter" type="radio" name="filter" value="-2" checked="checked"> &nbsp;All &nbsp; &nbsp;
+            <input class="Filter" type="radio" name="filter" value="1"> &nbsp;Active &nbsp; &nbsp;
+            <input class="Filter" type="radio" name="filter" value="0"> &nbsp;Inactive &nbsp; &nbsp;
+          </div> -->
+          <!-- <div class="col-xs-12 col-sm-1"></div> -->
+
+          <div class="col-xs-12 col-sm-12">
+            <!--<input type="text" id="search" name="search" class="form-control input-sm" placeholder="Search" value=""/> -->
+            <h3>Slider Top</h3>
+            <div>
+            <label>Selector 1:</label>
+            <select>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+            </select>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-2">
+           <!-- <button type="submit" class="btn btn-primary search_btn"><i class="fa fa-search"></i> Search</button> -->
+          </div>
+        </div>
+             
+      </div><!-- /.box-header -->
+          
+      <div class="divide20"></div>
+      <div class="box-body no-padding">
+        <div id="content">
+         <!-- <h4 class="text-center">Please wait</h4> -->
+        </div>
+      </div>
+
+          <div class="box-footer"></div>
+      </div>
+
+  </section>
+</div>
+<?php require_once ('theme/footer.php');?>
+
+<script>
+
+
+/*$(document).ready(function() {
+    $('.search_btn').click(function() {
+        search();
+    });
+    
+    $('#search').keyup(function(e) {
+       // ****************e.which==13 for enter key working ****************************
+        if(e.which==13){ 
+          pagination(1,5,1);
+            search();
+        }
+         // ****************e.which==27 for ESC key working ****************************
+        else if(e.which==27)
+        {
+            $('#search').val("");
+            pagination(1,5,1);
+            search();
+        }
+        
+    });
+    
+});*/
+
+/*$("body").on("change",".Filter",function(){
+  search();
+});*/
+
+
+/*function search(){
+    var searchkey = $('#search').val();
+       var status=$('input:radio[name=filter]:checked').val();
+       var page=$(".active1").attr("page");
+       
+        $.ajax({
+            type: 'POST',
+            url: '../adminapi/image/image_list.php',
+            data: {
+                search:searchkey,
+                status:status,
+                page:page,
+                limit:10
+                },
+            success: function(data) {
+              console.log(data);
+              print_data(data);
+            }
+        });
+}*/
+ 
+/*function print_data(data){
+  // console.log(fetchdata);
+  var arr=JSON.parse(data);
+
+  if(arr.status=="success"){
+
+    var i;
+    var out='';
+
+    var image_count=arr.image_list.length;
+    if(image_count!=0){
+      //atleast one row return
+      //out+='<div class="box box-primary"><div class="box-body table-responsive no-padding">';
+
+      out+='<table class="table table-striped"><tbody>';
+      out+='<tr>';
+      out+='<th class="col-sm-2">Category name</th>';
+      out+='<th class="col-sm-3">Name</th>';
+      out+='<th class="col-sm-6">Description</th>';
+      out+='<th class="col-sm-1">Action</th>';
+      out+='</tr>';
+      for(i=0;i<image_count;i++){
+        out+='<tr>';
+
+        out+='<td>';
+        if(arr.image_list[i].category_status=="1")
+          out+='<a href="image_category_detail.php?id='+arr.image_list[i].category_id+'" class="text-success">'+arr.image_list[i].category_name+'</a>';
+        else out+='<a href="image_category_detail.php?id='+arr.image_list[i].category_id+'" class="text-danger">'+arr.image_list[i].category_name+'</a>';
+        out+='</td>';
+
+        out+='<td>';
+        if(arr.image_list[i].status=="1")
+          out+='<a href="image_detail.php?id='+arr.image_list[i].image_id+'" class="text-success">'+arr.image_list[i].name+'</a>';
+        else out+='<a href="image_detail.php?id='+arr.image_list[i].image_id+'" class="text-danger">'+arr.image_list[i].name+'</a>';
+        out+='</td>';
+
+        out+='<td>'+arr.image_list[i].description+'</td>';
+
+        out+='<td><div class="row">';
+        if(arr.image_list[i].status=="1"){
+          //category is in active mode. press this to deactivate
+          out+='<a class="btn btn-danger btn-block" onclick="toggleStatus('+arr.image_list[i].image_id+')" data-toggle="tooltip" title="click for deactivate"><i class="fa fa-times"></i>&nbsp;Active</a>';
+        }else{
+          //category is in inactive mode. press this to activate
+          out+='<a class="btn btn-success btn-block" onclick="toggleStatus('+arr.image_list[i].image_id+')" data-toggle="tooltip" title="click for activate"><i class="fa fa-check"></i>&nbsp;Inactive</a>';
+        }
+        //out+='&nbsp;<a href="video_edit.php?id='+arr.video_list[i].video_id+'" class="btn btn-warning" data-toggle="tooltip" title="Edit Video"><i class="fa fa-pencil"></i></a>';
+        out+='</div></td>';
+
+        out+='</tr>';
+      }
+      out+="</tbody></table>";//</div></div>";
+    }else{
+      //no coupon return
+      out+='<h4 class="text-center text-danger">No data return</h4>';
+    }
+  }
+  else{
+    out='<h3 class="text-center text-danger">'+arr.remark+'</h3>';
+  }
+      
+  document.getElementById("content").innerHTML=out;
+  
+}
+*/
+//pagination(1,5,1);
+  
+
+$("body").on("click",".page",function()
+{
+  // alert("click");
+  // $("li").removeClass("active");
+  var start=1;
+  var page=$(this).attr("page");
+  if(page>2)
+    start=page-2;
+
+  pagination(start,start+4,page);
+  search();
+});
+
+$("body").ready(function(){
+     search();
+  });
+
+function toggleStatus(image_id){
+  swal({
+    title: "Are you sure ?",
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "green",
+    confirmButtonText: "Yes",
+    closeOnConfirm: true
+  },
+  function(){
+    $.ajax({
+            type: 'POST',
+            url: '../adminapi/image/image_change_status.php',
+            data: {
+                  image_id:image_id
+                },
+            success: function(data) {
+              console.log(data);
+              var arr=JSON.parse(data);
+              if(arr["status"]=="success"){
+                toast("Changes made successfully");
+                search();
+              }else{
+                swal("Error",arr["remark"],"error");
+              }
+            }
+        });
+  });
+}
+
+</script>
--- userAPI/user/search.php
+++ userAPI/user/search.php
@@ -11,7 +11,7 @@ $count5=0;
 $count6=0;
 $count7=0;
 
-$a='{"status":"Success",';
+$output='{"status":"Success"';
 $audio="";
 $category_audio="";
 $category_video="";
@@ -20,14 +20,21 @@ $event="";
 $image="";
 $video="";
 
+$audio_arr=array();
+$category_audio_arr=array();
+$category_image_arr=array();
+$category_video_arr=array();
+$event_arr=array();
+$image_arr=array();
+$video_arr=array();
+$blog_arr=array();
+$c=array();
+
 if(isset($_REQUEST["query"])){  
     $min_length = 3;
     if(strlen($query) >= $min_length){
     	$query = htmlspecialchars($query); 
-        // changes characters used in html to their equivalents, for example: < to &gt;
-         
-        //$query = mysql_real_escape_string($query);
-        // makes sure nobody uses SQL injection
+        
 
 
         //$raw_results = mysqli_query("SELECT * FROM audio WHERE (`name` LIKE '%".$query."%')")or die(mysql_error());
@@ -36,36 +43,24 @@ if(isset($_REQUEST["query"])){
       	$row_count=mysqli_num_rows($result);
         if($row_count > 0){ // if one or more rows are returned do following
              
-            //while($results = mysqli_fetch_array($raw_results)){
-            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-             
-                //echo "<p><h3>".$results['name']."</h3></p>";
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
-            //}
-            //echo "OK";
+            
             $flag=1;
             
-            while($row = $result->fetch_assoc()) {
+            while($row = $result->fetch_assoc()) 
+            {
         		
-        		//$audio.= "{".$row["name"]."}";
-        		//$audio[]='$row["name"]';
-        		//echo $a;
-        		//array_push($audio, $row["name"]);
-        		if($count1==0){
-        			$audio.= "{".$row["name"]."}";
-        		}
-        		elseif ($count1==1) {
-        			$audio.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$audio.= ","."{".$row["name"]."}";	
-        		}
-        		$count1++;
+        		
+                $audio_arr[] = $row;
+                // $audio.=json_encode($c);
+
     		}
     		
     		
         }
-       
+        
+        $output.=', "audio":'; 
+        $output.= json_encode($audio_arr);
+
         $raw_results="select * from `category_audio` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
       	$row_count=mysqli_num_rows($result);
@@ -74,24 +69,15 @@ if(isset($_REQUEST["query"])){
            
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$category_audio.= "{".$row["name"]."}";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count2==0){
-        			$category_audio.= "{".$row["name"]."}";
-        		}
-        		elseif ($count2==1) {
-        			$category_audio.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$category_audio.= ","."{".$row["name"]."}";	
-        		}
-        		$count2++;
+        		
+                $category_audio_arr[]=$row;
     		}
     		
         }
        
+        $output .=', "category_audio":' ;
+        $output.= json_encode($category_audio_arr);
+
         $raw_results="select * from `category_image` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
       	$row_count=mysqli_num_rows($result);
@@ -100,24 +86,17 @@ if(isset($_REQUEST["query"])){
            
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$category_image.= "{".$row["name"]."}";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count3==0){
-        			$category_image.= "{".$row["name"]."}";
-        		}
-        		elseif ($count3==1) {
-        			$category_image.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$category_image.= ","."{".$row["name"]."}";	
-        		}
-        		$count3++;
+        		
+                $category_image_arr[]=$row;
+
+
     		}
     		
     		
         }
+
+        $output .=', "category_image":' ;
+        $output.= json_encode($category_image_arr);
         
         $raw_results="select * from `category_video` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
@@ -127,25 +106,16 @@ if(isset($_REQUEST["query"])){
             
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$category_video.= "{".$row["name"]."}";
-        		//$a.= $row["name"].",";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count4==0){
-        			$category_video.= "{".$row["name"]."}";
-        		}
-        		elseif ($count4==1) {
-        			$category_video.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$category_video.= ","."{".$row["name"]."}";	
-        		}
-        		$count4++;
+        		
+                $category_video_arr[]=$row;
+
     		}
     		
     		
         }
+
+        $output .=', "category_video":' ;
+        $output.= json_encode($category_video_arr);
         
         $raw_results="select * from `event` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
@@ -155,23 +125,15 @@ if(isset($_REQUEST["query"])){
             
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$event.= "{".$row["name"]."}";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count5==0){
-        			$event.= "{".$row["name"]."}";
-        		}
-        		elseif ($count5==1) {
-        			$event.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$event.= ","."{".$row["name"]."}";	
-        		}
-        		$count5++;
+        		
+                $event_arr[]=$row;
+
     		}
     		
         }
+        $output .=', "event":' ;
+        $output.= json_encode($event_arr);
+
         $raw_results="select * from `image` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
       	$row_count=mysqli_num_rows($result);
@@ -180,23 +142,18 @@ if(isset($_REQUEST["query"])){
             
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$image.= "{".$row["name"]."}";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count6==0){
-        			$image.= "{".$row["name"]."}";
-        		}
-        		elseif ($count6==1) {
-        			$image.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$image.= ","."{".$row["name"]."}";	
-        		}
-        		$count6++;
+        		
+
+                $image_arr[]=$row;
+
+
     		}
     		
         }
+
+        $output .=', "image":'; 
+        $output.= json_encode($image_arr);
+
         $raw_results="select * from `video` where `name` like '%".$query."%' or 'description' like '%".$query."%'";
         $result=mysqli_query($con,$raw_results);
       	$row_count=mysqli_num_rows($result);
@@ -205,23 +162,35 @@ if(isset($_REQUEST["query"])){
             
             $flag=1;
             while($row = $result->fetch_assoc()) {
-        		//$a='{"status":"Success","result":"';
-        		//$video.= "{".$row["name"]."}";
-        		//$a.='"}';
-        		//echo $a;
-        		if($count7==0){
-        			$video.= "{".$row["name"]."}";
-        		}
-        		elseif ($count7==1) {
-        			$video.= ","."{".$row["name"]."}";
-        		}
-        		else{
-        			$video.= ","."{".$row["name"]."}";	
-        		}
-        		$count7++;
+        		
+                $video_arr[]=$row;
+
+
     		}
     		
         }
+        $output .=', "video":';
+        $output.= json_encode($video_arr);
+
+
+
+        $raw_results="select * from `blog` where `blog_title` like '%".$query."%' or 'blog_description' like '%".$query."%'";
+        $result=mysqli_query($con,$raw_results);
+        $row_count=mysqli_num_rows($result);
+        if($row_count > 0){ // if one or more rows are returned do following
+             
+            
+            $flag=1;
+            while($row = $result->fetch_assoc()) {
+                
+                $blog_arr[]=$row;
+
+
+            }
+            
+        }
+        $output .=', "blog":';
+        $output.= json_encode($blog_arr);
              
     }
     else{
@@ -235,17 +204,9 @@ if(isset($_REQUEST["query"])){
         }
         elseif($flag==1){
 
-        	$a.='"audio":'.'"['.$audio.']"';
-        	$a.=',"category_audio":'.'"['.$category_audio.']"';
-        	$a.=',"category_image":'.'"['.$category_image.']"';
-        	$a.=',"category_video":'.'"['.$category_video.']"';
-        	$a.=',"event":'.'"['.$event.']"';
-        	$a.=',"image":'.'"['.$image.']"';
-        	$a.=',"video":'.'"['.$video.']"';
-
-
-        	$a.='}'."\r\n";
-        	echo $a;
+        	// $a.='}'."\r\n";
+            $output.='}'."\r\n";
+        	echo $output;
         }
 }
 
